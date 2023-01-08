7a7216e03888846f84bcf10e8f76372f20918334
Alexander <sany199574@gmail.com>
2023-01-09 01:21:49 +0300

    Создан базовый сервис
    Создали класс ApiRequest для нашего запроса
    
    Реализовали универсальный метод для всех API внутри BaseService
    -SendAsync

---
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Web/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using static PS.MangoRestaurant.Web.SD;
+
+namespace PS.MangoRestaurant.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; } = null!;
+        public object Data { get; set; } = null!;
+        public string AccessToken { get; set; } = null!;
+    }
+}
---
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using PS.MangoRestaurant.Web.Models;
+
+namespace PS.MangoRestaurant.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        public ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
