d26f1f5cee835f82b909d8e2e6b71de47d594316
Alexander <sany199574@gmail.com>
2023-01-15 02:08:55 +0300

    Создан контроллер ProductController
    -реализован метод ProductIndex
    -исправлена ошибка, убран лишний слэш

---
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PS.MangoRestaurant.Web.Models;
+using PS.MangoRestaurant.Web.Services.IServices;
+
+namespace PS.MangoRestaurant.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if(response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
