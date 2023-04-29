a842b92c5b63cb57f496b833ee9a642d04742feb
Alexander <sany199574@gmail.com>
2023-04-29 18:09:10 +0300

    Добавлена миграция

--- src/PS.MangoRestaurant/PS.MangoRestaurant.Services.CouponAPI/Models/Coupon.cs
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Services.CouponAPI/Models/Coupon.cs
@@ -6,7 +6,7 @@ namespace PS.MangoRestaurant.Services.CouponAPI.Models
     {
         [Key]
         public int CouponId { get; set; }
-        public string CouponCode { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
         public double DiscountAmount { get; set; }
     }
 }
