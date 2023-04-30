1d61fc673ad92c97f05372388579f3b96db0512f
Alexander <sany199574@gmail.com>
2023-04-30 16:15:23 +0300

    реализация методов работы с купонами в корзину покупок

--- src/PS.MangoRestaurant/PS.MangoRestaurant.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -115,5 +115,24 @@ namespace PS.MangoRestaurant.Services.ShoppingCartAPI.Repository
             }
             return false;
         }
+
+
+        public async Task<bool> ApplyCoupon(string userId, string coupon)
+        {
+            var cartFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserId == userId);
+            cartFromDb!.CouponCode = coupon;
+            _db.CartHeaders.Update(cartFromDb);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> RemoveCoupon(string userId)
+        {
+            var cartFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserId == userId);
+            cartFromDb!.CouponCode = "";
+            _db.CartHeaders.Update(cartFromDb);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
     }
 }
