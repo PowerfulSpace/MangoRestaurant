1887f023a85505ae35978fa723beb9ea32704452
Alexander <sany199574@gmail.com>
2023-05-09 22:38:47 +0300

    Реализация репозитория заказов

--- src/PS.MangoRestaurant/PS.MangoRestaurant.Services.OrderAPI/Repository/OrderRepository.cs
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Services.OrderAPI/Repository/OrderRepository.cs
@@ -1,17 +1,42 @@
-﻿using PS.MangoRestaurant.Services.OrderAPI.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PS.MangoRestaurant.Services.OrderAPI.DbContexts;
+using PS.MangoRestaurant.Services.OrderAPI.Models;
 
 namespace PS.MangoRestaurant.Services.OrderAPI.Repository
 {
     public class OrderRepository : IOrderRepository
     {
-        public Task<bool> AddOrder(OrderHeader orderHeader)
+        private readonly DbContextOptions<ApplicationDbContext> _dbContext;
+
+        public OrderRepository(DbContextOptions<ApplicationDbContext> dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
         }
 
-        public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await using var _db = new ApplicationDbContext(_dbContext);
+                _db.orderHeaders.Add(orderHeader);
+                await _db.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            await using var _db = new ApplicationDbContext(_dbContext);
+            var orderHeaderFromDb = await _db.orderHeaders.FirstOrDefaultAsync(u => u.OrderHeaderId == orderHeaderId);
+            if(orderHeaderFromDb != null)
+            {
+                orderHeaderFromDb.PaymentStatus = paid;
+                await _db.SaveChangesAsync();
+            }
         }
     }
 }
