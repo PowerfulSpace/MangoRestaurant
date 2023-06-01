a37640f7139d262236794581c3aa37a53808697a
Alexander <sany199574@gmail.com>
2023-06-01 18:01:04 +0300

    Создание модели дял обработки платежа PaymentRequestMessage

---
+++ src/PS.MangoRestaurant/PS.MangoRestaurant.Services.OrderAPI/Messages/PaymentRequestMessage.cs
@@ -0,0 +1,14 @@
+﻿using PS.MangoRestaurant.MessageBus;
+
+namespace PS.MangoRestaurant.Services.OrderAPI.Messages
+{
+    public class PaymentRequestMessage : BaseMessage
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public string ExpiryMonthYear { get; set; } = string.Empty;
+        public double OrderTotal { get; set; }
+    }
+}
